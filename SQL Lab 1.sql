eab2838edde8bf7b939ed0cf6563ad657085fc53
Peter George Ngugulu <71594734+petergeorge649@users.noreply.github.com>
2021-04-06 16:37:23 +0200

    Add files via upload

---
+++ SQL Lab 1.sql
@@ -0,0 +1,37 @@
+show tables; #Review the tables in the database
+# SELECT * FROM actor; 
+# SELECT * FROM actor_info; 
+# SELECT * FROM address; 
+# SELECT * FROM category; 
+# SELECT * FROM city; 
+# SELECT * FROM country; 
+# SELECT * FROM customer; 
+# SELECT * FROM film; 
+# SELECT * FROM film_actor; 
+# SELECT * FROM film_category; 
+# SELECT * FROM film_text; 
+# SELECT * FROM inventory; 
+# SELECT * FROM language; 
+# SELECT * FROM payment; 
+# SELECT * FROM rental; 
+# SELECT * FROM staff; 
+# SELECT * FROM store; 
+
+# SELECT `actor_id` as globalMovie, first_name as fn
+#FROM actor;
+
+# SELECT distinct name as language 
+#FROM language;        
+
+#SELECT COUNT(rental_date) FROM rental;
+
+#SELECT COUNT(staff_id) FROM staff;
+
+#SELECT COUNT(store_id) FROM store;
+
+#SELECT `staff_id`, first_name
+#FROM `staff`;
+
+#SELECT `first_name` FROM `staff`;
+
+#SELECT (rental_date) FROM `rental`
